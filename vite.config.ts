bf7ce6d4fcbfa488990612acbde580b00cfa1a2e
Sarthak Kadu <sarthakkadu67@gmail.com>
2025-12-15 11:58:48 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true
+    host: true,
+    allowedHosts: true
   },
   build: {
     outDir: 'dist',
